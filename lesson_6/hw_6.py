897b099f7abc9d2f7efd1427918f7fc44eb0d8a6
Bohdan Kostenko <77276415+BohdanKostenko@users.noreply.github.com>
2021-11-09 12:48:55 +0200

    Add def normalize

--- lesson_6/hw_6.py
+++ lesson_6/hw_6.py
@@ -60,16 +60,92 @@ async def sort_file(path_to_sorting, files_list):
                 continue
 
 
+async def normalize(path_to_sorting):
+
+    alphabet = {ord('А'): 'A',
+                ord('Б'): 'B',
+                ord('В'): 'V',
+                ord('Г'): 'G',
+                ord('Д'): 'D',
+                ord('Е'): 'E',
+                ord('Ё'): 'Je',
+                ord('Ж'): 'Zh',
+                ord('З'): 'Z',
+                ord('И'): 'I',
+                ord('Й'): 'Y',
+                ord('К'): 'K',
+                ord('Л'): 'L',
+                ord('М'): 'M',
+                ord('Н'): 'N',
+                ord('П'): 'P',
+                ord('Р'): 'R',
+                ord('С'): 'S',
+                ord('Т'): 'T',
+                ord('У'): 'U',
+                ord('Ф'): 'F',
+                ord('Х'): 'Kh',
+                ord('Ц'): 'C',
+                ord('Ч'): 'Ch',
+                ord('Ш'): 'Sh',
+                ord('Щ'): 'Jsh',
+                ord('Ъ'): 'Z',
+                ord('Ы'): 'Ih',
+                ord('Ь'): 'Jh',
+                ord('Э'): 'Eh',
+                ord('Ю'): 'Ju',
+                ord('Я'): 'Ja',
+                ord('а'): 'a',
+                ord('б'): 'b',
+                ord('в'): 'v',
+                ord('г'): 'g',
+                ord('д'): 'd',
+                ord('е'): 'e',
+                ord('ё'): 'je',
+                ord('ж'): 'zh',
+                ord('з'): 'z',
+                ord('и'): 'i',
+                ord('й'): 'y',
+                ord('к'): 'k',
+                ord('л'): 'l',
+                ord('м'): 'm',
+                ord('н'): 'n',
+                ord('п'): 'p',
+                ord('р'): 'r',
+                ord('с'): 's',
+                ord('т'): 't',
+                ord('у'): 'u',
+                ord('ф'): 'f',
+                ord('х'): 'kh',
+                ord('ц'): 'c',
+                ord('ч'): 'ch',
+                ord('ш'): 'sh',
+                ord('щ'): 'jsh',
+                ord('ъ'): 'z',
+                ord('ы'): 'ih',
+                ord('ь'): 'jh',
+                ord('э'): 'eh',
+                ord('ю'): 'ju',
+                ord('я'): 'ja'}
+    for root, dirs, files in os.walk(path_to_sorting):
+        for dir in dirs:
+            path_folder = os.path.join(root, dir)
+            if os.path.exists(path_folder):
+                os.rename(str(path_folder), str(path_folder).translate(alphabet))
+        for file in files:
+            path_file = os.path.join(root, file)
+            if os.path.exists(path_file):
+                os.rename(str(path_file), str(path_file).translate(alphabet))
+
+
 async def main():
     files_list = list()
     try:
-        # path_to_sorting = AsyncPath(sys.argv[1])
-        path_to_sorting = AsyncPath(r"C:\Users\bohda\OneDrive\Документы\GitHub\goit-python-web\lesson_6\t")
+        path_to_sorting = AsyncPath(sys.argv[1])
     except Exception:
         print("Wrong! Please try again")
-
     print(f'Started in {path_to_sorting}')
     start = time()
+    await normalize(path_to_sorting)
     await search_file(path_to_sorting, files_list)
     await sort_file(path_to_sorting, files_list)
     for root, dirs, files in os.walk(path_to_sorting):
