9d336e4f46811d6af5fde906448ece781a7de83d
Bohdan Kostenko <77276415+BohdanKostenko@users.noreply.github.com>
2021-11-21 12:40:52 +0200

    upload test version

--- lesson_7/client.py
+++ lesson_7/client.py
@@ -1,16 +1,38 @@
 import socket
+import threading
 
 server = (socket.gethostbyname(socket.gethostname()), 8000)
+flag_close = True
 
 
-def client():
-    with socket.socket() as soc:
-        soc.bind(("", 0))
-        soc.connect(server)
-        soc.sendto(b"Hello", server)
-        data = soc.recv(1024)
-        print(data.decode())
+def client_receives_msg():
+    global flag_close
+    while flag_close:
+        try:
+            data = soc.recv(1024)
+            print(data.decode('utf-8'))
+        except:
+            flag_close = False
 
 
-if __name__ == '__main__':
-    client()
+with socket.socket() as soc:
+    soc.bind(("", 0))
+    soc.connect(server)
+    alias = input("Nickname ")  # Вводим наш псевдоним
+    soc.sendto(("[" + alias + '] Connect to chat').encode('utf-8'), server)  # Уведомляем сервер о подключении
+    potok = threading.Thread(target=client_receives_msg)
+    potok.start()
+
+    while flag_close:
+        try:
+            message = input("Message: ")
+            soc.sendto(("[" + alias + '] ' + message).encode('utf-8'), server)
+        except KeyboardInterrupt:
+            soc.sendto(('[' + alias + '] left chat').encode('utf-8'), server)
+            flag_close = False
+
+    potok.join()
+
+
+
+
--- lesson_7/serv.py
+++ lesson_7/serv.py
@@ -1,26 +1,43 @@
 import socket
+import time
 
 host = socket.gethostbyname(socket.gethostname())
 port = 8000
 print(host)
-def server():
 
-    with socket.socket() as sock:
-        sock.bind((host, port))
-        sock.listen(10)
-        print("Started Server")
+clients = list()
+
+
+with socket.socket() as sock:
+    sock.bind((host, port))
+    sock.listen(10)
+    print("Started Server")
+    while True:
         conn, addr = sock.accept()
         print(f'Connection {addr}')
+
+        if addr not in clients:
+            clients.append(addr)
+        connection_time = time.strftime("%H:%M:%S %d-%m-%Y", time.localtime())
+
         while True:
-            with conn:
-                try:
-                    request = conn.recv(1024)
-                    print(request.decode())
-                    conn.sendall(b"Ok is connect!")
-                except:
-                    print("Stopped Server")
+            try:
+                request = conn.recv(1024)
+                if not request:
                     break
+                # print(request.decode())
+                # conn.sendall(b"Ok is connect!")
 
+                print(">>> IP:" + addr[0] + " PORT:" + str(addr[1]) + " " + connection_time + " ", end="")
+                print(request.decode("utf-8"))
+                sock.sendto(b"Ok conected", addr)
 
-if __name__ == '__main__':
-    server()
+                for client in clients:
+                    if addr != client:
+                        try:
+                            sock.send(request, client)
+                        except:
+                            print(" error send")
+
+            except KeyboardInterrupt:
+                print("Stooped sever")
